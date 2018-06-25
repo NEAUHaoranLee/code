810dea35c72eb7995c81dcc80bac2da64dc5b16c
NEAUHaoranLee <2642582817@qq.com>
2018-06-25 12:54:26 +0800

    6/25

--- 整站开发/personalvue/src/components/Contect.vue
+++ 整站开发/personalvue/src/components/Contect.vue
@@ -12,12 +12,15 @@
        </div>
     <div id="contect-me">
         <div id="el-container" >
-           <div class="el" v-for="(item,index) in el" @mouseover="mo" :key="index">
+           <a class="el" v-for="(item,index) in el" :key="index" :href="item.href">
+             <transition name="fade">
+             <img :src="item.click" alt="" class="click" v-show="isShow==item.name" >
+             </transition>
+             <img :src="item.path" alt="" class="img" @click="show(item.name)">  
              <div class="details" >
-               <img src="../../static/img/contect/.png" alt="">
-               <span>{{item}}</span>
+               <span>{{item.name}}</span>
              </div>
-             </div>
+             </a>
         </div>
     </div>
 
@@ -30,12 +33,43 @@ export default {
     return {
       c1: "rgb(255, 140, 0)",
       c2: "rgb(255, 72, 0)",
-      el: ["GitHub", "email", "QQ", "weChat", "weibo"]
+      el: [
+        {
+          name: "GitHub",
+          path: "../../static/img/contect/github.png",
+          click: "",
+          href: "https://github.com/NEAUHaoranLee/"
+        },
+        {
+          name: "QQ",
+          path: "../../static/img/contect/qq.png",
+          click: "../../static/img/contect/q-code.jpg",
+          href: "#"
+        },
+        {
+          name: "weChat",
+          path: "../../static/img/contect/weChat.png",
+          click: "../../static/img/contect/v-code.png",
+          href: "#"
+        },
+        {
+          name: "weibo",
+          path: "../../static/img/contect/weibo.png",
+          click: "",
+          href: ""
+        }
+      ],
+      isShow: ""
     };
   },
   methods: {
-    mo: function(e) {
-      console.log(e.target);
+    show: function(name) {
+      console.log(name);
+      if (this.isShow == name) {
+        this.isShow = "";
+      } else {
+        this.isShow = name;
+      }
     }
   }
 };
@@ -67,19 +101,33 @@ export default {
 }
 #contect-me {
   /* border-top: 1px solid #ffffff; */
-  width: 350px;
+  width: 280px;
   margin: 0 auto;
 }
 #el-container {
   height: 50px;
-  width: 350px;
+  /* width: 350px; */
   margin: 0 auto;
+}
+.img {
+  position: absolute;
+  width: 40px;
+  height: 40px;
+}
+.click {
+  position: absolute;
+  width: 150px;
+  bottom: 50px;
+  left: 50%;
+  transform: translateX(-50%);
+  transition: 0.3s;
+  box-shadow: 0px 0px 5px #061ad6;
 }
 .el {
   width: 40px;
   height: 40px;
   border-radius: 50%;
-  border: 0.1px solid #ffffff;
+  /* border: 0.1px solid #ffffff; */
   float: left;
   margin: 10px 15px 0;
   position: relative;
@@ -87,7 +135,7 @@ export default {
 }
 .details {
   position: absolute;
-  height: 0px;
+  /* height: 0px; */
   width: 70px;
   top: 40px;
   left: -13px;
@@ -95,8 +143,19 @@ export default {
   color: #ffffff;
   text-align: center;
   transition: 0.3s;
+  /* visibility: hidden; */
+  opacity: 0;
 }
 .el:hover .details {
-  height: 20px;
+  /* visibility: visible;  */
+  opacity: 1;
+}
+.fade-enter-active,
+.fade-leave {
+  opacity: 1;
+}
+.fade-leave-active,
+.fade-enter {
+  opacity: 0;
 }
 </style>
